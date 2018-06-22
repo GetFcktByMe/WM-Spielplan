cc7f798fec3eed3788df3287d47e2174c127f3a5
Patrick Patrickovic Lehmannovic <patrickl.ehmann307@gmail.com>
2018-06-22 11:12:13 +0200

    neu

--- Teams.java
+++ Teams.java
@@ -8,27 +8,28 @@
 public class Teams
 {
     // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private String Teamname;
+    private String teamname;
     private int spieler;
     private int punkte;
     private int platzierung;
     private int tore;
     private int gegentore;
-    private int torverhältniss;
+    private int torverhältnis;
     
 
     /**
      * Konstruktor für Objekte der Klasse Teams
      */
-    public Teams()
+    public Teams(int spieler, String teamname, int punkte, int platzierung, int tore, int gegentore, int torverhältnis )
     {
-        
+       this.spieler = spieler;
+       this.punkte = punkte;
+       this.platzierung = platzierung;
+       this.tore = tore;
+       this.gegentore = gegentore;
+       this.torverhältnis = torverhältnis;
+       this.teamname = teamname;
     }
 
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-   
+    
+}
